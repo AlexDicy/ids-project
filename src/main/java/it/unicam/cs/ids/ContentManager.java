a74d93360636cbe357c784a1e53bfcb0575db7c7
Leonardo Salpini <leonardo.salpini00@gmail.com>
2024-01-26 14:12:47 +0100

    added overload method for submit and getInDateRange in ContentManager interface

--- src/main/java/it/unicam/cs/ids/ContentManager.java
+++ src/main/java/it/unicam/cs/ids/ContentManager.java
@@ -2,6 +2,7 @@ package it.unicam.cs.ids;
 
 import it.unicam.cs.ids.model.Content;
 
+import java.util.Date;
 import java.util.List;
 
 public interface ContentManager<C extends Content> {
@@ -9,6 +10,7 @@ public interface ContentManager<C extends Content> {
     C get(String id);
 
     void submit(C content);
+    void submit(List<C> content);
 
     List<C> getAll();
 
@@ -19,4 +21,6 @@ public interface ContentManager<C extends Content> {
     void remove(String id);
 
     List<C> getContentToApprove();
+
+    List<C> getInDateRange(Date start, Date end);
 }
