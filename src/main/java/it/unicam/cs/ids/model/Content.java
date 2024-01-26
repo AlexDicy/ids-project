6f6d86ce323f759e3fb0afde59d79a0e2a7e3a10
Leonardo Salpini <leonardo.salpini00@gmail.com>
2024-01-26 12:44:20 +0100

    updated Content class based on the changes made on visual paradigm: added createdBy and creationDate attributes.

--- src/main/java/it/unicam/cs/ids/model/Content.java
+++ src/main/java/it/unicam/cs/ids/model/Content.java
@@ -1,18 +1,22 @@
 package it.unicam.cs.ids.model;
 
+import java.util.Date;
+
 public abstract class Content {
     private final String id;
     private final String name;
     private final String description;
     private boolean approved;
-    private final String author;
+    private final String createdBy;
+    private final Date creationDate;
 
-    protected Content(String id, String name, String description, String author, boolean approved) {
+    protected Content(String id, String name, String description, String createdBy, boolean approved, Date creationDate) {
         this.id = id;
         this.name = name;
         this.description = description;
-        this.author = author;
         this.approved = approved;
+        this.createdBy = createdBy;
+        this.creationDate = creationDate;
     }
 
     public String getId() {
@@ -35,7 +39,11 @@ public abstract class Content {
         this.approved = approved;
     }
 
-    public String getAuthor() {
-        return author;
+    public String getAuthorId() {
+        return this.createdBy;
+    }
+
+    public Date getCreationDate() {
+        return this.creationDate;
     }
 }
