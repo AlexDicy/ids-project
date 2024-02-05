7889219941f3b8ccec78e6f2f6ed7adaeccafea4
Lorenzo Lapucci <halexercraft@gmail.com>
2024-02-05 11:18:41 +0100

    Create frontend base files

---
+++ frontend/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="p-4 h-[80vh]">
+    <div class="text-4xl">
+      TownExplorer
+    </div>
+    <div class="flex w-full h-full justify-center items-center text-2xl">
+      Work in progress
+    </div>
+  </div>
+</template>
